3feec182eb08f1b98f29b51aff93bcecce6a8bd7
Clay <claystonebarongo@gmail.com>
2026-03-02 22:08:36 +0300

    initial upload

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react'
+
+import './App.css'
+
+function App() {
+  const movie_Url = `https://picsum.photos/v2/list?page=1&limit=20`
+  const [movieList, setMovieList] = useState([])
+
+  const fetchData = async () => {
+    await fetch(movie_Url).then(res => res.json()).then(data => setMovieList(data))
+  }
+
+  useEffect(() => {
+    fetchData(); // call fetchData when component mounts
+  }, []);
+
+  return (
+    <>
+      <div className="grid">
+
+        {movieList.map((movie) => {
+          return (
+            <>
+              <div className="bold">
+                <p>pictures</p>
+                <img src={movie.download_url} alt="yoo" />
+              </div>
+            </>
+          )
+
+        })}
+      </div>
+
+    </>
+  )
+}
+
+export default App
