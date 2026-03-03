029688bed5d4b99a1cfdbc228eb5149aa1003b81
Clay <claystonebarongo@gmail.com>
2026-03-03 23:49:46 +0300

    daily updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState, useEffect } from 'react'
 
 import './App.css'
 import Navbar from './pages/Navbar/Navbar'
+import Feed from './pages/Feed/Feed'
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Feed />
+
     </div>
   )
 
--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import pic1 from '../../../public/Pasted image.png'
+import pic1 from '../../assets/Pasted image.png'
 import menu from '../../assets/menu.png'
 import './Navbar.css'
 const Navbar = () => {
@@ -7,7 +7,7 @@ const Navbar = () => {
         <div className='Navbar'>
             <img src={menu} alt="" className='menu' />
             <img src={pic1} alt="" className='logo' />
-            <h1>MubaCast</h1>
+            <h1>MubaCast.</h1>
             <input type="text" placeholder='Search Movies/Tv Shows' className="movie-search" />
             <button>Download App</button>
 
