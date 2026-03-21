42df626f8e48f334d9192df40327fdaba12661a1
Clay <claystonebarongo@gmail.com>
2026-03-21 22:24:11 +0300

    daily updates

--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -7,8 +7,9 @@ const Navbar = ({ toggleSidebar, setSearchQuery }) => {
         <div className='Navbar'>
             <img src={menu} alt="" className='menu' onClick={toggleSidebar} />
             <img src={pic1} alt="" className='logo' />
-            <h1>MubaCast. <span>Recommendation Site</span></h1>
+            <h1>MubaCast. </h1>
             <input type="text" placeholder='Get Movies/Tv Shows Recommendations' className="movie-search" onChange={(e) => setSearchQuery(e.target.value)} />
+
             <button>Download App</button>
 
         </div>
