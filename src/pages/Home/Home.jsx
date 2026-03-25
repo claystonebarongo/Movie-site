bede94fc55618da6c23169d665d53cc50ee5713b
Clay <claystonebarongo@gmail.com>
2026-03-25 23:16:30 +0300

    daily updates

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,6 +14,7 @@ const Home = ({ category, searchQuery }) => {
         let url = "";
 
         if (searchQuery) {
+
             url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${searchQuery}`;
         } else {
 
