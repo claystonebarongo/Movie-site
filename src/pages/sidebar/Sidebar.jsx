2e2661e713411c7479a499a7fdcd2ede87e61934
Clay <claystonebarongo@gmail.com>
2026-03-28 23:25:29 +0300

    daily updates

--- src/pages/sidebar/Sidebar.jsx
+++ src/pages/sidebar/Sidebar.jsx
@@ -16,6 +16,35 @@ const Sidebar = ({ openSidebar, setCategory }) => {
     return (
         <div className={openSidebar ? "Side" : "Side2"}>
 
+            <ul>
+                <li onClick={() => setCategory(0)}>
+                    <Home size={18} /> Home
+                </li>
+                <li onClick={() => setCategory(28)}>
+                    <Zap size={18} /> Action
+                </li>
+                <li onClick={() => setCategory(12)}>
+                    <Map size={18} /> Adventure
+                </li>
+                <li onClick={() => setCategory(35)}>
+                    <Laugh size={18} /> Comedy
+                </li>
+                <li onClick={() => setCategory(18)}>
+                    <Drama size={18} /> Drama
+                </li>
+                <li onClick={() => setCategory(27)}>
+                    <Ghost size={18} /> Horror
+                </li>
+                <li onClick={() => setCategory(10749)}>
+                    <Heart size={18} /> Romance
+                </li>
+                <li onClick={() => setCategory(53)}>
+                    <AlertTriangle size={18} /> Thriller
+                </li>
+                <li onClick={() => setCategory(878)}>
+                    <Rocket size={18} /> Sci-Fi
+                </li>
+            </ul>
         </div>
     )
 }
